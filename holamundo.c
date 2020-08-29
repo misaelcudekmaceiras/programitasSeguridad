03decea218d0565845dd09dcdc2b36bce2da85af
Misael <misaelcudek@gmail.com>
2020-08-29 19:16:23 -0300

    Add files via upload
    /*Programa para analizar la memoria de un proceso utilizando pmap
    Para compilar gcc -o eje holamundoc.
    Para ejecutar ./eje 
    
    0)Corra el programa
    1)Luego ejecute en la consola los comandos  $ pmap  `pidof eje`
    Se sugiere revisar en profundidad con  pmap -x `pidof eje` ;  pmap -X `pidof eje` ; pmap -XX `pidof eje`
    */

---
+++ holamundo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+/*Programa para analizar la memoria de un proceso
+Para compilar gcc -o eje holamundoc.
+Para ejecutar ./eje 
+
+0)Corra el programa
+1)Luego ejecute en la consola los comandos  $ pmap  `pidof eje`
+Se sugiere revisar en profundidad con  pmap -x `pidof eje` ;  pmap -X `pidof eje` ; pmap -XX `pidof eje`
+*/
+
+int main()
+{
+
+    printf("Hola Mundo!!\n");
+    getchar();//para que no finalice el programa
+    
+}
