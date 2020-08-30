fedfaeecc90501e5b4901eed587eded02e688a05
misa <misaelcudek@gmail.com>
2020-08-30 14:26:05 -0300

    agrego la salida

--- ProblemasConStrings/usomalprintf.c
+++ ProblemasConStrings/usomalprintf.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+/*
+ Para compilar: gcc -o eje usomalprintf.c
+ Para ejecutar ./ejecutar
+ El fin de este programa es mostar como la funcion printf
+ continua leyendo la memoria e imprimiendo en la consola
+ 
+ En la consola podria ver:
 
+usando bien al printf por 1 vez
+usando mal printf 1° usando mal printf 1° por 1 vez
+
+usando mal printf 2° usando mal printf 2° usando mal printf 1° por 1 vez
+
+ 
+ 
+ */
 
 int main()
 {
