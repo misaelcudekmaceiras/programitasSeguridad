46da5630cc1c6e32868c303e9c209802a1afd49a
misa <misaelcudek@gmail.com>
2020-08-30 14:19:18 -0300

    Mal Uso de Strings

---
+++ ProblemasConStrings/usomalprintf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main()
+{
+    printf("usando bien %s printf por %d vez\n","al",1);
+    printf("usando mal printf 1° %s\n");
+    printf("usando mal printf 2° %s\n");
+    
+    return(0);
+}
